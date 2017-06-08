c61d6d476025af8d5fafeb595847b3d23388baf0
Chipdev <mehcubed@gmail.com>
2017-06-07 17:48:53 -0700

    Initiation of GenericCommand implementations with the use of an ArrayList.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,24 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.util.DiscordException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by blake on 6/6/17.
  */
 public class Main {
 
+    public static EventDispatcher dispatcher;
+    public static List<GenericCommand> genericCommandList = new ArrayList<GenericCommand>();
+
     //Code taken from https://github.com/austinv11/Discord4J just incase you are from the future and lost context. Thanks.
 
     public static void main(String[] args){
         IDiscordClient client = createClient(args[0], true);
-        EventDispatcher dispatcher = client.getDispatcher(); // Gets the EventDispatcher instance for this client instance
-        dispatcher.registerListener(new PingCommand());
+        dispatcher = client.getDispatcher(); // Gets the EventDispatcher instance for this client instance
+        genericCommandList.add(new PingCommand());
+
     }
 
     public static IDiscordClient createClient(String token, boolean login) { // Returns a new instance of the Discord client
@@ -28,6 +35,12 @@ public class Main {
         } catch (DiscordException e) { // This is thrown if there was a problem building the client
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public static void initiateCommandsFromArray() {
+        for(int i = 0; i < genericCommandList.size() - 1; i++){
+            dispatcher.registerListener(genericCommandList.get(i));
         }
     }
 
